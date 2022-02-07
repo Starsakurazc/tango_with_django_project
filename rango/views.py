ef44b714dd26d056dcb27b7ea4673931a09b7e7f
Sakurazc <2584876z@student.gla.ac.uk>
2022-02-07 22:04:52 +0000

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -9,15 +9,17 @@ from rango.forms import UserForm, UserProfileForm
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'categories': category_list,
                     'pages': page_list}
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
 
 @login_required()
@@ -78,8 +80,9 @@ def add_page(request, category_name_slug):
 
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Jiacheng'}
-    return render(request, 'rango/about.html', context=context_dict)
+    context_dict = {'boldmessage': {'This tutorial has been put together by Jiacheng'},
+                    'visits': request.session['visits']}
+    return render(request, 'rango/about.html', context_dict)
 
 
 def register(request):
@@ -130,7 +133,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return render(request,'rango/restricted.html')
+    return render(request, 'rango/restricted.html')
 
 
 @login_required
@@ -139,3 +142,20 @@ def user_logout(request):
     return redirect(reverse('rango:index'))
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -126,3 +126,5 @@ MEDIA_ROOT = MEDIA_DIR
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
+
+LOGIN_URL = 'rango:login'
